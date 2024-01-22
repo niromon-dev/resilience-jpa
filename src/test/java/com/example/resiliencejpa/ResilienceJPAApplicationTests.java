402c33544e9d794d99aa8288cb1aee1dc1e64b94
niromon-dev <morin.arnaud@gmail.com>
2024-01-22 15:00:37 +0100

    add performance test

--- src/test/java/com/example/resiliencejpa/ResilienceJPAApplicationTests.java
+++ src/test/java/com/example/resiliencejpa/ResilienceJPAApplicationTests.java
@@ -1,13 +1,10 @@
 package com.example.resiliencejpa;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//@SpringBootTest
 class ResilienceJPAApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    //@Test
+    void contextLoads() {
+    }
 
 }
